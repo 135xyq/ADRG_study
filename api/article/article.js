d34d6d2c295b6c05a5cfb6f412f8ebde6ddec8eb
xyq <xyq135@gmail.com>
2023-03-30 14:44:40 +0800

    课程  文章详情  展示文章

--- api/article/article.js
+++ api/article/article.js
@@ -4,11 +4,15 @@ import request from "@/utils/request.js"
 
 export default {
 	// 根据分类id获取文章列表
-	getList(data){
-		return request.post('/applet/article/list',data);
+	getList(data) {
+		return request.post('/applet/article/list', data);
 	},
 	// 分页获取
-	getPage(data){
-		return request.post('/applet/article/page',data);
+	getPage(data) {
+		return request.post('/applet/article/page', data);
+	},
+	// 获取文章详情
+	getDetail(data) {
+		return request.post('/applet/article/detail', data);
 	}
 }
--- pages/class/articleread/articleread.vue
+++ pages/class/articleread/articleread.vue
@@ -1,24 +1,70 @@
 <template>
 	<view>
-		articleread
+		<!-- <u-loading-page loading-text="文章加载中...." :loading="loading"></u-loading-page> -->
+			<u-skeleton
+			    rows="30"
+			    title
+				:loading="loading"
+			></u-skeleton>
+		<view class="markdown-container">
+			<u-parse :content="articleContent"></u-parse>
+		</view>
 	</view>
 </template>
 
 <script>
+	import articleApi from "@/api/article/article.js"
+	import MarkdownIt from 'markdown-it'
+
 	export default {
 		data() {
 			return {
-				id:'' ,// 文章id
+				id: '', // 文章id
+				article: {},
+				articleContent: '',
+				loading:false
 			};
 		},
-		onLoad(option) {
+		async onLoad(option) {
 			// 获取文章id
 			this.id = option.id;
-			console.log('文章',this.id);
+
+			if (!this.id) {
+				// 文章id有误跳回原页面
+				uni.navigateBack()
+			} else {
+				await this.getArticleDetail();
+			}
+		},
+		methods: {
+			/**
+			 * 获取文章详情
+			 */
+			async getArticleDetail() {
+				// 展示骨架屏
+				this.loading = true 
+				
+				const res = await articleApi.getDetail({
+					id: this.id
+				});
+
+				this.article = res.data;
+
+				const md = new MarkdownIt()
+				this.articleContent = md.render(this.article.content)
+				
+				this.loading = false
+
+			}
 		}
 	}
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+	.markdown-container {
+		padding: 24rpx;
+		font-size: 28rpx;
+		color: $u-content-color;
+		line-height: 1.6;
+	}
 </style>
