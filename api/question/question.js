0dfbf32ca703b617ea4535d70ef233ef44f33ebb
xyq <xyq135@gmail.com>
2023-04-17 14:48:02 +0800

    题库 判题、判题结果、还能出题的api

--- api/question/question.js
+++ api/question/question.js
@@ -8,5 +8,17 @@ export default{
 	// 随机组卷
 	getRandomQuestions(data) {
 		return request.post('/applet/question/getRandomQuestions',data);
+	},
+	// 判断是否还能组卷，能出得题目数量不为0
+	isHasQuestion(data) {
+		return request.post('/applet/question/ishasquestion',data);
+	},
+	// 交卷判题
+	postQuestionToValidate(data) {
+		return request.post('/applet/question/validatequestionanswer',data);
+	},
+	// 获取判题的结果
+	getQuestionRecordValidateResult(data) {
+		return request.post('/applet/question/getvalidateresult',data);
 	}
 }
